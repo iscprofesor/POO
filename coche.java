6475f5c932212f66d2f8927cb1101e12b241bd2d
iscprofesor <106636766+iscprofesor@users.noreply.github.com>
2024-03-07 09:21:57 -0600

    Update coche.java

--- coche.java
+++ coche.java
@@ -10,10 +10,16 @@ class Coche
    // Método
    public static void main(String[] args)
     {
+     
+     // Crear objeto.
      Coche coche1 = new Coche();
+     
+     // Modificar atributos. 
      coche1.color = "Rojo";
      coche1.marca = "Mitsubishi";
      coche1.km = 0;
+     
+     // Mostrar por pantalla valores del objeto
      System.out.println("El color del coche es: "+coche1.color);
      System.out.println("La marca del coche es: "+coche1.marca);
      System.out.println("El km del coche es: "+coche1.km);
